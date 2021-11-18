60b12f97bea75489a1dec47627ee177b356e7ccd
Cristian Sena <cristianjsena@gmail.com>
2021-11-17 23:12:19 -0300

    Agrego tests

---
+++ __test__/integration/api.test.js
@@ -0,0 +1,7 @@
+const request = require('supertest');
+const app = require('../../src');
+
+describe('API Rest de productos', () => {
+
+
+});
---
+++ __test__/unit/producto.test.js
@@ -0,0 +1,25 @@
+
+
+describe('Verifica respuestas a la lectura, incorporación, modificación y borrado de productos: ', () => {
+
+    test('Lectura productos', () => {
+
+    });
+
+    test('Lectura de un producto', () => {
+
+    });
+
+    test('Incorporación de un producto', () => {
+
+    });
+
+    test('Modificación de un producto', () => {
+
+    });
+
+    test('Borrado de un producto', () => {
+
+    });
+
+});
